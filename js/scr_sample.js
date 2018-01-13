ce67f97bcb6efd50944d8d6f2f69d43ae7e5640d
Liu Wei <liuweiky@qq.com>
2018-01-13 17:37:35 +0800

    更新

---
+++ js/scr_sample.js
@@ -0,0 +1,9 @@
+document.write('<script src="https://speech.uinnova.com/static/lib/ace-builds/src-noconflict/ace.js"></script>\
+<script src="https://speech.uinnova.com/static/lib/ace-builds/src-noconflict/ext-language_tools.js"></script>\
+<!-- <script src="../lib/ScriptEditor/assets/plugin/ace-builds/src-noconflict/ace.js"></script> -->\
+<!-- <script src="../lib/ScriptEditor/assets/plugin/ace-builds/src-noconflict/ext-language_tools.js"></script> -->\
+<script src="../lib/ScriptEditor/assets/plugin/jquery/js/jquery-latest.js"></script>\
+<script src="../js/sample.js"></script>\
+<script src="../lib/ScriptEditor/javascripts/nodejs.js"></script>\
+<script src="../lib/ScriptEditor/demos/demos.js"></script>\
+<!-- <script src="../lib/ScriptEditor/javascripts/editor.js"></script> -->');
